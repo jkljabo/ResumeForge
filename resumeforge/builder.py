d446bd1db23f7b9946cf440b8978d2b0e3a3548a
jkljabo <jason.k.little@comcast.net>
2026-08-25 10:29:24 -0400

    Implement extensible rendering pipeline

--- resumeforge/builder.py
+++ resumeforge/builder.py
@@ -5,13 +5,19 @@ from resumeforge.sections import summary
 
 
 class ResumeBuilder:
-
     def __init__(self):
         self.document = Document()
+        self.renderers = [
+            header.render,
+            summary.render,
+        ]
 
     def render(self, resume):
-        header.render(self.document, resume)
-        summary.render(self.document, resume)
+        for renderer in self.renderers:
+            renderer(self.document, resume)
+
+    def add_renderer(self, renderer):
+        self.renderers.append(renderer)
 
     def save(self, filename):
         self.document.save(filename)
